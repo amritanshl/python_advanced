5b05e3c529e070fa6ef2988329f4ff0fdd401a12
amritanshl <amritanshlal@gmail.com>
2025-12-09 14:01:24 +0530

    fdsadfsa

--- selen.py
+++ selen.py
@@ -1,13 +1,23 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
 
 driver = webdriver.Chrome()
-driver.get("https://acme-test.uipath.com/")
+wait = WebDriverWait(driver, 30)
+
 driver.implicitly_wait(10)
+driver.get("https://acme-test.uipath.com/")
+
+button_submit = wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@type='submit']")))
 email_element = driver.find_element(By.ID,"email")
 email_element.send_keys("amritanshlal@gmail.com")
-time.sleep(5)
+
 pass_element = driver.find_element(By.NAME,"password")
 pass_element.send_keys("Welcome@1234")
+
+button_submit.click()
 time.sleep(30)
